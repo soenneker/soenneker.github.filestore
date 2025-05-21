6334be5bb69adf4ba1f01c1efba0bae91a31bac2
Jake Soenneker <jake@soenneker.com>
2025-05-21 12:53:36 -0500

    interface mods

--- src/Abstract/IGitHubFileStore.cs
+++ src/Abstract/IGitHubFileStore.cs
@@ -29,6 +29,14 @@ public interface IGitHubFileStore
     /// <returns>The file contents as a string.</returns>
     ValueTask<string> Read(string owner, string repo, string path, CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Reads the contents of a file.
+    /// </summary>
+    /// <param name="owner">The repository owner.</param>
+    /// <param name="repo">The repository name.</param>
+    /// <param name="path">The path to the file.</param>
+    /// <param name="cancellationToken">Optional cancellation token.</param>
+    /// <returns>The file contents as a string.</returns>
     ValueTask<byte[]> ReadAsBytes(string owner, string repo, string path, CancellationToken cancellationToken = default);
 
     /// <summary>
@@ -48,6 +56,22 @@ public interface IGitHubFileStore
         string? authorName = null, string? authorEmail = null, CancellationToken cancellationToken = default);
 
     /// <summary>
+    /// Creates a new file.
+    /// </summary>
+    /// <param name="owner">The repository owner.</param>
+    /// <param name="repo">The repository name.</param>
+    /// <param name="path">The path where to create the file.</param>
+    /// <param name="content">The file content.</param>
+    /// <param name="message">Optional commit message. If not provided, an automated message will be used.</param>
+    /// <param name="branch">Optional branch name. Defaults to "main".</param>
+    /// <param name="authorEmail"></param>
+    /// <param name="cancellationToken">Optional cancellation token.</param>
+    /// <param name="authorName"></param>
+    /// <returns>The file commit information.</returns>
+    ValueTask<FileCommit?> WriteBytes(string owner, string repo, string path, byte[] content, string? message = null, string branch = "main",
+        string? authorName = null, string? authorEmail = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
     /// Deletes a file.
     /// </summary>
     /// <param name="owner">The repository owner.</param>
@@ -59,8 +83,8 @@ public interface IGitHubFileStore
     /// <param name="cancellationToken">Optional cancellation token.</param>
     /// <param name="authorName"></param>
     /// <returns>The file commit information.</returns>
-    ValueTask<FileCommit?> Delete(string owner, string repo, string path, string? message = null, string branch = "main",
-        string? authorName = null, string? authorEmail = null, CancellationToken cancellationToken = default);
+    ValueTask<FileCommit?> Delete(string owner, string repo, string path, string? message = null, string branch = "main", string? authorName = null,
+        string? authorEmail = null, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Lists the contents of a directory.
